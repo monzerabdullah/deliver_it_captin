5f96a91d6f585c8678ef3939f8c1512e38d5b7c0
Monzer Abdullah <monzerabdullah.dev@gmail.com>
2024-07-08 12:02:33 +0200

    orders view is created and set to no orders yet

--- lib/views/orders_view.dart
+++ lib/views/orders_view.dart
@@ -1,13 +1,31 @@
+import 'package:deliver_it_captin/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class Orders extends StatelessWidget {
   const Orders({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text('Orders Page'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SvgPicture.asset('images/svgs/history.svg'),
+            const SizedBox(
+              height: 45,
+            ),
+            const Text(
+              'لا توجد رحلات سابقة اليوم',
+              style: TextStyle(
+                fontFamily: 'Cairo',
+                color: kSecondaryText,
+                fontSize: 22.0,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
